331c044a767bbc4035353747e26f0ab4a61e4081
selmahi.mail@gmail.com <Soukaina@air-de-loic.home>
2025-05-20 11:03:13 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // app.js
 const express = require("express");
 const app = express();
-const usersRouter = require("./routes/usersRouter");
+const router = require("./routes/Router");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("/", usersRouter);
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Express app listening on port ${PORT}!`));
---
+++ routes/Router.js
@@ -0,0 +1,10 @@
+// routes/Router.js
+const { Router } = require("express");
+const Controller = require("../controllers/Controller");
+const router = Router();
+
+router.get("/", Controller.getUsernames);
+router.get("/new", Controller.createUsernameGet);
+router.post("/new", Controller.createUsernamePost);
+
+module.exports = router;
